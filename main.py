4e55c1eb27a302d24aabfa86175e691095e88cbd
user <vramm19@ya.ru>
2025-01-07 21:23:24 +0300

    correct time

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import httpx
 from config import TRAVELPOUT_API_KEY # Укажите ваш API ключ TravelPayouts
+from datetime import datetime, timezone, timedelta
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -83,11 +84,17 @@ async def get_flight_info(from_city: str, to_city: str, departure_date: str):
         flight_duration = flight_data.get('duration', None)
         price = flight_data.get('price', "Неизвестная цена")
         airline = flight_data.get('airline', "Неизвестная авиакомпания")
-        departure_at = flight_data.get('departure_at', "Неизвестное время вылета")
+        departure_at_str = flight_data.get('departure_at', "Неизвестное время вылета")
+
+        # Преобразуем строку в объект datetime
+        departure_at = datetime.fromisoformat(departure_at_str.replace("Z", "+00:00"))
+
+        # Форматируем дату и время
+        formatted_departure_at = "* " + departure_at.strftime("%d/%m/%Y") + " " + departure_at.strftime("%H:%M") + " ч. по МСК *"
 
         return {
             "duration": flight_duration,
             "price": price,
             "airline": airline,
-            "departure_at": departure_at
+            "departure_at": formatted_departure_at  # Используем отформатированное время
         }
